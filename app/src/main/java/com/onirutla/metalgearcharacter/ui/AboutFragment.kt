cb8af058b56a54f69c7df0fe2cb3ca7da8106d8d
onirutlA <alturino001@gmail.com>
2021-10-06 13:02:12 +0700

    add: navigate back to toolbar

--- app/src/main/java/com/onirutla/metalgearcharacter/ui/AboutFragment.kt
+++ app/src/main/java/com/onirutla/metalgearcharacter/ui/AboutFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.onirutla.metalgearcharacter.databinding.FragmentAboutBinding
 
 class AboutFragment : Fragment() {
@@ -18,6 +19,13 @@ class AboutFragment : Fragment() {
     ): View {
         _binding = FragmentAboutBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.toolbar.setNavigationOnClickListener {
+            findNavController().navigateUp()
+        }
     }
 
 
