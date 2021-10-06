3d1e6b2e06d9979f22f8e7d9611eddce5b06c467
onirutlA <alturino001@gmail.com>
2021-10-07 06:47:55 +0700

    add: enter and return transition in about fragment

--- app/src/main/java/com/onirutla/metalgearcharacter/ui/AboutFragment.kt
+++ app/src/main/java/com/onirutla/metalgearcharacter/ui/AboutFragment.kt
@@ -6,12 +6,24 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.transition.MaterialSharedAxis
+import com.onirutla.metalgearcharacter.R
 import com.onirutla.metalgearcharacter.databinding.FragmentAboutBinding
 
 class AboutFragment : Fragment() {
 
     private var _binding: FragmentAboutBinding? = null
     private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enterTransition = MaterialSharedAxis(MaterialSharedAxis.Z, true).apply {
+            duration = resources.getInteger(R.integer.material_motion_duration_long_1).toLong()
+        }
+        returnTransition = MaterialSharedAxis(MaterialSharedAxis.Z, false).apply {
+            duration = resources.getInteger(R.integer.material_motion_duration_long_1).toLong()
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
